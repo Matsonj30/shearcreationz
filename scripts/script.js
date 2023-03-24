1d5c48ef61a51c40b8a1e0058a6350504cd4402e
Matsonj30 <matsonj2013@gmail.com>
2023-03-23 22:54:00 -0600

    hero complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,14 @@
 function burgerMenu(){
     var checkbox = document.getElementById("hamburgerCheckbox");
     var menu = document.getElementById("menu");
-    var overflow = document.getElementById("overFlowMenu");
     var burgerMenu = document.getElementsByClassName("hamburgerMenu");
 
     if(checkbox.checked == true){
         menu.style.transform = "none";
-        overflow.style.zIndex = "5";
         burgerMenu[0].style.position = "fixed";
     }
     else{
         menu.style.transform = "translate(200px)";
-        overflow.style.zIndex = "1";
         burgerMenu[0].style.position = "absolute";
     }
 }
