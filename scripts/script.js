4f864805edad38c901fd1e06fe5ff67306fbbd6e
Matsonj30 <matsonj2013@gmail.com>
2023-03-25 19:41:36 -0600

    start of testimonial

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,24 @@ function burgerMenu(){
         burgerMenu[0].style.position = "absolute";
     }
 }
+
+function moveTestimonial(direction){
+    var currentTestimonialIndex = null;
+    for(i = 1; i <= 3; i+=1){
+        console.log("testimonial"+[i])
+        var currentTestimonial = document.getElementById("testimonial"+[i])
+        if(currentTestimonial.style.display != 'none'){
+            currentTestimonial.style.transform = 'translate(100vw)';
+            currentTestimonialIndex = i;
+            break
+        }
+    }
+
+    if(direction == 'left'){
+        
+    }
+    else{
+
+    }
+
+}
